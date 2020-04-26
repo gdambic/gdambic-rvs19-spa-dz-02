c4848cd7ce8e5700553804b2bd47bd2efe259376
Barta150 <mbartolic150@gmail.com>
2020-04-26 20:29:10 +0200

    commit

---
+++ SpaDomacaZadaca02/Gofl.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+class Gofl
+{
+public:
+	Gofl(sf::RenderWindow* window);
+	void draw();
+private:
+	void setTime();
+	void text();
+	void pickColor();
+	void change();
+	void drawBoard();
+	sf::RenderWindow* window;
+	sf::Clock frameClock;
+	int color[40][40];
+	bool passed = true;
+	sf::Time stop = sf::seconds(1.00f);
+
+};
+
